5bfd7e7773c9a8eff4a7c1913a2049b55f84f181
Rafal Urbanski (Sintel) <rafal@sintel.ie>
2019-10-28 19:50:25 +0100

    Added typescript support; Added babel polyfill;

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'eval-source-map',
-    entry: './dev/index.js',
+    entry: {
+        index: ["@babel/polyfill/noConflict", './dev/index.ts']
+    },
     devServer: {
         port: 8080,
         contentBase: path.join(__dirname, "dist")
@@ -14,63 +16,63 @@ module.exports = {
     // },
     module: {
         rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [{
-                        // creates style nodes from JS strings
-                        loader: "style-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // translates CSS into CommonJS
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // compiles Sass to CSS
-                        loader: "sass-loader",
-                        options: {
-                            outputStyle: 'expanded',
-                            sourceMap: true,
-                            sourceMapContents: true
-                        }
-                    }
-                    // Please note we are not running postcss here
-                ]
-            },
-            {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        // On development we want to see where the file is coming from, hence we preserve the [path]
-                        name: '[path][name].[ext]?hash=[hash:20]',
-                        limit: 8192
-                    }
-                }]
-            },
-            {
-                // Load all icons
-                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-                use: [{
-                    loader: 'file-loader',
-                }]
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.(scss|css)$/,
+            use: [{
+                // creates style nodes from JS strings
+                loader: "style-loader",
+                options: {
+                    sourceMap: true
+                }
+            }, {
+                // translates CSS into CommonJS
+                loader: "css-loader",
+                options: {
+                    sourceMap: true
+                }
+            }, {
+                // compiles Sass to CSS
+                loader: "sass-loader",
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMap: true,
+                    sourceMapContents: true
+                }
             }
-        ],
+                // Please note we are not running postcss here
+            ]
+        }, {
+            // Load all images as base64 encoding if they are smaller than 8192 bytes
+            test: /\.(png|jpg|gif)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    // On development we want to see where the file is coming from, hence we preserve the [path]
+                    name: '[path][name].[ext]?hash=[hash:20]',
+                    limit: 8192
+                }
+            }]
+        }, {
+            // Load all icons
+            test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+            use: [{
+                loader: 'file-loader',
+            }]
+        }, {
+            test: /\.tsx?$/,
+            use: 'awesome-typescript-loader',
+            exclude: /node_modules/
+        }],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './dev/index.template.html',
-            inject: true
-        })
+            inject: true,
+            chunks: ["index"],
+            filename: "index.html"
+        }),
     ]
 };
